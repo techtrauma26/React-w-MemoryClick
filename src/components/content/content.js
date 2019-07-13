6d4cb3dcbc54d3de51bcf5a4c455c192dd78a0c5
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-13 18:22:59 -0400

    added images

---
+++ src/components/content/content.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Content.css";
+
+const Content = props => (
+    <div className="card">
+      <div className="img-container">
+        <img className="img-thumbnail img-responsive" alt={props.name} src={props.image} onClick={() => props.memoryClick(props.id)}/>
+      </div>
+    </div>
+  );
+  export default Content;
+  
