53e8160f2a4b734a1dd16e1904ad2a49baf15940
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-14 05:50:00 -0400

    added content

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Content.css";
 
 const Content = props => (
-    <div className="card">
+    <div className="content">
       <div className="img-container">
         <img className="img-thumbnail img-responsive" alt={props.name} src={props.image} onClick={() => props.memoryClick(props.id)}/>
       </div>
