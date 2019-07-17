ae60f87678d0bb4b89700f13b94c9cd39ba087b7
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-17 01:24:27 -0400

    import

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,4 @@
-import React from './node_modules/react';
+import React from 'react';
 import './navbar.css';
 
 const navbar = props => <div className={props.shakeContent === "true" ? "shakeContainer" : "picContainer"}>{props.pictures}</div>;
