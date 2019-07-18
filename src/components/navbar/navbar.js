94da6232304cc059389a5b664baad02663f12a6d
Vyjoo Frank <vlakshm26@gmail.com>
2019-07-18 01:22:36 -0400

    fixed navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './navbar.css';
 
-const navbar = props => <div className={props.shakeContent === "true" ? "shakeContainer" : "picContainer"}>{props.pictures}</div>;
+const Navbar = props => <div className={props.shakeContent === "true" ? "shakeContainer" : "picContainer"}>{props.pictures}</div>;
 
-export default navbar;
+export default Navbar;
